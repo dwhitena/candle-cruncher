0bdc4bcbf786ba41598b9b1b10433483a1d83fa0
Daniel Whitenack <whitenack.daniel@gmail.com>
2020-11-16 07:21:26 -0500

    add number of pots conversion

--- candle_cruncher.py
+++ candle_cruncher.py
@@ -5,6 +5,8 @@ density = 0.884859
 
 st.title('Candle Cruncher 🕯️ ⚖️')
 
+st.header('Fragrance Weight')
+
 # Fragrance Option
 option = st.selectbox(
     'What fragrance are you mixing?',
@@ -12,7 +14,7 @@ option = st.selectbox(
 
 # Input Volume
 volume = st.number_input('Wax volume (in liters)', min_value=0.0, 
-        max_value=600.0, value=400.0, step=1.00, format=None, key=None)
+        max_value=600.0, value=270.0, step=1.00, format=None, key=None)
 
 # Calculate fragrance weight
 if option == 'Christmas Day':
@@ -24,6 +26,21 @@ else:
 
 # Convert kg to lbs
 weight = 2.20462*weight
+weight_pounds = int(weight)
+weight_ounces = weight*16.0 - float(weight_pounds)*16.0
 
 # Print the result
-st.success('Mix this much fragrance: %0.2f pounds (%0.2f oz)' % (weight, weight*16.0))
+st.success('Mix this much fragrance: %0.2f lbs %0.2f oz' % (weight_pounds, weight_ounces))
+
+st.header('Target Volume')
+
+# Input Pots
+
+pots= st.number_input('Number of pots', min_value=1.0, 
+        max_value=110.0, value=80.0, step=1.00, format=None, key=None)
+
+weight_kg = pots*1.81437
+target = weight_kg/0.884859
+
+# Print the result
+st.success('Your volume target is: %0.2f liters' % (target))
